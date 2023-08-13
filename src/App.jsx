a59891464033d4e054376248209633b9108bf49f
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2023-08-13 14:38:01 +0300

    add roll buttton

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 import React from "react"
 import Die from "./Die"
+import {nanoid} from "nanoid"
 
 export default function App(){
 
-    const[dice, setDice] = React.useState([])
+    const[dice, setDice] = React.useState(generateDices())
     
-    function giveDiceNumbers(){
-       return Math.floor(Math.random() * 6) + 1
+    function fillDiceData(){
+       return{
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid()
+       }
     }
+
+    function generateDices(){
+        const arr = []
+        for(let i = 0; i < 10; i++)
+        {
+            arr[i] = fillDiceData();
+        }
+        return arr
+    }
+    
+    function HoldDice(){
+        console.log("hold")
+    }
+
+   const diceElements = dice.map(die => (
+      <Die key={die.id}
+      value={die.value} 
+      isHeld={die.isHeld}
+      id={die.id}
+      HoldDice = {HoldDice} />
+   )
+   )
+   
+    console.log(dice.value)
     return(
         <div className="centreItems">
         <main>
             <div className="diceContainer"> 
                <div className="diceHolder">
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
-               <Die value={giveDiceNumbers()}/>
+                {diceElements}
                </div>
-               <button> Roll</button>
+               <button onClick={() => setDice(generateDices)}> Roll</button>
             </div>       
         </main>
         </div>
--- src/Die.jsx
+++ src/Die.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Die(props){
     return(
-        <div className="dice">
+        <div className="dice" onClick={props.HoldDice}>
             <h1>{props.value}</h1>
         </div>
     )
